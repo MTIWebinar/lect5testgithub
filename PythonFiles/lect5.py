65dcfb24d3a3edec1eb4e68bab4193bd66753fb0
Kirill Osipov <osipovka@gmail.com>
2014-01-11 12:13:52 +0400

    [nf] add class

--- PythonFiles/lect5.py
+++ PythonFiles/lect5.py
@@ -1,10 +1,24 @@
 # -*- coding:utf-8 -*-
 
+class Person(object):
+	"""docstring for Person"""
 
+	obj_id = 0
+	name = '***'
+
+	def __init__(self, obj_id=1, name='AAaa'):
+		super(Person, self).__init__()
+		self.obj_id = obj_id
+		self.name = name
+
+	def __str__(self):
+		return '{}{}'.format(self.obj_id, self.name)
 
 
 def main():
-	print 'hello'
+	person = []
+	vasya = Person(1, 'Vasya')
+	print vasya
 
 if __name__ == '__main__':
 	main()
